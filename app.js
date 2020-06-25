783598672957b95ca5d5528813a229123c7b04f2
cmooredev <cmooredev@pm.me>
2020-06-25 15:50:25 -0700

    Basic logic implemented for maze generation

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse} = Matter;
+const { 
+    Engine, 
+    Render, 
+    Runner, 
+    World, 
+    Bodies
+} = Matter;
+
+const cells = 3;
+const width = 600;
+const height = 600;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -6,31 +16,49 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        width: 800,
-        height: 600
+        wireframes: true,
+        width: width,
+        height: height
     }
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas)
-}));
-
 //Walls
 const walls = [
-    Bodies.rectangle(400, 0, 800, 40, {
+    Bodies.rectangle(width/2, 0, width, 40, {
         isStatic: true
     }),
-    Bodies.rectangle(400, 600, 800, 40, {
+    Bodies.rectangle(width/2, height, width, 40, {
         isStatic: true
     }),
-    Bodies.rectangle(0, 300, 40, 600, {
+    Bodies.rectangle(0, height/2, 40, height, {
         isStatic: true
     }),
-    Bodies.rectangle(800, 300, 40, 600, {
+    Bodies.rectangle(width, height/2, 40, height, {
         isStatic: true
-    }),
-    Bodies.rectangle(200,200,50,50)
+    })
 ];
 World.add(world, walls);
+
+//Maze generation
+
+const grid = Array(cells)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+    .fill(null)
+    .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+    .fill(null)
+    .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+    
+};
+stepThroughCell(startRow, startColumn);
